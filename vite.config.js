9ce7ac5a55c86f5763e6d6479d99d45bbb603514
ditorbejo <ditoooriq@gmail.com>
2022-06-24 17:18:33 +0700

    Mengubah manifest.json kedalam vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,60 @@ export default defineConfig({
     vue(),
     VitePWA({
       includeAssets: [
-        "favicon.svg",
-        "favicon.ico",
+        "favicon3.png",
         "robots.txt",
-        "apple-touch-icon.png",
       ],
+      manifest: {
+        name: "WalkSummit",
+        short_name: "Mountain Hiking",
+        description: "Platform for hiking",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#d84315",
+        icons: [
+          {
+            src: "/icon-pwa/android-chrome-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/icon-pwa/android-chrome-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/icon-pwa/android-chrome-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/icon-pwa/android-chrome-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/icon-pwa/android-chrome-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/icon-pwa/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-pwa/android-chrome-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-pwa/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
       workbox: {
         sourcemap: true,
       },
