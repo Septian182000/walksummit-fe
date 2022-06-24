73d069d4ff4723907ddbe4ff4b7242d4f4f4d3bd
ditorbejo <ditoooriq@gmail.com>
2022-06-24 11:33:13 +0700

    Menambahkan PWA, namun kurang asset icon pada vite.config manifest

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,57 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "WalkSummit",
+        short_name: "Mountain Hiking",
+        description: "Platform For Hiking",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "./public/icon-pwa/android/android-launchericon-192-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./public/icon-pwa/android-launchericon-512-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "./public/icon-pwa/android-launchericon-512-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        sourcemap: true,
+      },
+      registerType: "autoUpdate",
+      strategies: "injectManifest",
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
